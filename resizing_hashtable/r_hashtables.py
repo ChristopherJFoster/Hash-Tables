2dd4da02620f4b7c28ffb48d55825bd823e712bc
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-07-09 19:01:38 -0400

    Stretch: r_hashtables.py HashTable now resizes automatically when load factor is greater than 0.7.

--- resizing_hashtable/r_hashtables.py
+++ resizing_hashtable/r_hashtables.py
@@ -1,8 +1,10 @@
-
+import copy
 
 # '''
 # Linked List hash table key/value pair
 # '''
+
+
 class LinkedPair:
     def __init__(self, key, value):
         self.key = key
@@ -19,11 +21,30 @@ class HashTable:
     def __init__(self, capacity):
         self.capacity = capacity
         self.storage = [None] * capacity
+        self.count = 0
+
+    def resize(self):
+        self.capacity = self.capacity * 2
+        temp_storage = copy.deepcopy(self.storage)
+        self.storage = [None] * self.capacity
+
+        for slot in temp_storage:
+            if slot != None:
+                def linked_list_rec(hash_table, current_pair):
+                    hash_table_insert(
+                        self, current_pair.key, current_pair.value, True)
+                    if current_pair.next != None:
+                        linked_list_rec(hash_table, current_pair.next)
+                    else:
+                        return
+                linked_list_rec(self, slot)
 
 
 # '''
 # Research and implement the djb2 hash function
 # '''
+
+
 def hash(string, max):
     hash = 5381
     for l in string:
@@ -36,7 +57,8 @@ def hash(string, max):
 
 # Hint: Use the LL to handle collisions
 # '''
-def hash_table_insert(hash_table, key, value):
+def hash_table_insert(hash_table, key, value, resize=False):
+    hash_table.count += 1
     if hash_table.storage[hash(key, hash_table.capacity)] == None or hash_table.storage[hash(key, hash_table.capacity)].key == key:
         hash_table.storage[hash(key, hash_table.capacity)
                            ] = LinkedPair(key, value)
@@ -50,6 +72,9 @@ def hash_table_insert(hash_table, key, value):
         linked_list_rec(hash_table, hash_table.storage[hash(
             key, hash_table.capacity)], key, value)
 
+    if hash_table.count / hash_table.capacity > 0.7 and resize == False:
+        hash_table.resize()
+
 
 # '''
 # Fill this in.
@@ -60,6 +85,7 @@ def hash_table_remove(hash_table, key):
     if hash_table.storage[hash(key, hash_table.capacity)] == None:
         print('Warning: supplied key does not exist')
     elif hash_table.storage[hash(key, hash_table.capacity)].key == key:
+        hash_table.count -= 1
         hash_table.storage[hash(key, hash_table.capacity)] = hash_table.storage[hash(
             key, hash_table.capacity)].next
     else:
@@ -68,6 +94,7 @@ def hash_table_remove(hash_table, key):
                 print('Warning: supplied key does not exist')
                 return
             elif current_pair.next.key == key:
+                hash_table.count -= 1
                 current_pair.next = current_pair.next.next
                 return
             else:
@@ -84,16 +111,11 @@ def hash_table_remove(hash_table, key):
 
 def hash_table_retrieve(hash_table, key):
     return_value = None
-    # print('hash(key, hash_table.capacity):',
-    #       hash(key, hash_table.capacity))
     if hash_table.storage[hash(key, hash_table.capacity)] != None:
         def linked_list_rec(hash_table, current_pair, key):
             nonlocal return_value
-            # print('current_pair.key, key: ', current_pair.key, key)
             if current_pair.key == key:
-                # print('success?', current_pair.value)
                 return_value = current_pair.value
-                # print('return_value: ', return_value)
             elif current_pair.next != None:
                 linked_list_rec(hash_table, current_pair.next, key)
             else:
@@ -116,7 +138,7 @@ def hash_table_resize(hash_table):
             def linked_list_rec(hash_table, current_pair):
                 nonlocal temp_table
                 hash_table_insert(
-                    temp_table, current_pair.key, current_pair.value)
+                    temp_table, current_pair.key, current_pair.value, True)
                 if current_pair.next != None:
                     linked_list_rec(hash_table, current_pair.next)
                 else:
@@ -130,8 +152,11 @@ def Testing():
     ht = HashTable(2)
 
     hash_table_insert(ht, "line_1", "Tiny hash table")
+    print('count: ', ht.count, 'capacity: ', ht.capacity)
     hash_table_insert(ht, "line_2", "Filled beyond capacity")
+    print('count: ', ht.count, 'capacity: ', ht.capacity)
     hash_table_insert(ht, "line_3", "Linked list saves the day!")
+    print('count: ', ht.count, 'capacity: ', ht.capacity)
 
     print(hash_table_retrieve(ht, "line_1"))
     print(hash_table_retrieve(ht, "line_2"))
